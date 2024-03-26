c986c262093a2230a6a442005caf7b4842660acf
lasalesi <larsericmueller@gmail.com>
2024-03-26 22:42:04 +0100

    calendar color and planned end

--- psag/psag/doctype/service_event/service_event.py
+++ psag/psag/doctype/service_event/service_event.py
@@ -5,22 +5,37 @@
 from __future__ import unicode_literals
 import frappe
 from frappe.model.document import Document
+import json
 
 class ServiceEvent(Document):
-	pass
+    pass
 
 @frappe.whitelist()
-def get_service_events():
-	events = frappe.db.sql("""SELECT
-		`tabService Event`.`name`,
-		`tabService Event`.`planned_start`,
-		`tabService Event`.`planned_end`,
-		`tabService Event`.`application_site_name`,
-		`tabService Event`.`responsible`,
-		`tabService Event`.`status`,
-		`tabResponsible`.`color` AS `color`
-		FROM `tabService Event`
-		LEFT JOIN `tabResponsible` ON `tabResponsible`.`name` = `tabService Event`.`responsible`""", as_dict=True)
-	
-	return events
-	
+def get_service_events(doctype, start, end, field_map, filters=None, fields=None):
+    field_map = frappe._dict(json.loads(field_map))
+    doc_meta = frappe.get_meta(doctype)
+    for d in doc_meta.fields:
+        if d.fieldtype == "Color":
+            field_map.update({
+                "color": d.fieldname
+            })
+
+    if filters:
+        filters = json.loads(filters or '')
+
+    if not fields:
+        fields = [field_map.start, field_map.end, field_map.title, 'name']
+
+    if field_map.color:
+        fields.append(field_map.color)
+
+    start_date = "ifnull(%s, '0001-01-01 00:00:00')" % field_map.start
+    end_date = "ifnull(%s, '2199-12-31 00:00:00')" % field_map.end
+
+    filters += [
+        [doctype, start_date, '<=', end],
+        [doctype, end_date, '>=', start],
+    ]
+
+    return frappe.get_list(doctype, fields=fields, filters=filters)
+
--- psag/psag/doctype/service_event/service_event_calendar.js
+++ psag/psag/doctype/service_event/service_event_calendar.js
@@ -1,28 +1,43 @@
+// Copyright (c) 2021-2024, libracore AG and contributors
+// For license information, please see license.txt
 frappe.views.calendar["Service Event"] = {
-	field_map: {
-		"start": "planned_start",
-		"end": "planned_end",
-		"id": "name",
-		"allDay": "all_day",
-		"title": "application_site_name",
-		"status": "status",
-		"eventColor": "color"
-	},
-	style_map: {
-		"Public": "success",
-		"Private": "info"
-	},
-	color_map: {
- 		"yellow": "yellow",
- 		"red": "red"
- 	},
-	filters: [
-		{
-			"fieldtype": "Link",
-			"fieldname": "responsible",
-			"options": "Responsible",
-			"label": __("Responsible")
-		}
-	],
-	get_events_method: "psag.psag.doctype.service_event.service_event.get_service_events"
+    field_map: {
+        "start": "planned_start",
+        "end": "planned_end",
+        "id": "name",
+        "allDay": "all_day",
+        "title": "application_site_name",
+        "status": "status",
+        "color": "color",
+        "child_name": "responsible"
+    },
+    style_map: {
+        "Public": "success",
+        "Private": "info"
+    },
+    color_map: {
+        "yellow": "yellow",
+        "red": "red"
+    },
+    filters: [
+        {
+            "fieldtype": "Link",
+            "fieldname": "application_site",
+            "options": "Application Site",
+            "label": __("Application Site")
+        },
+        {
+            "fieldtype": "Link",
+            "fieldname": "customer",
+            "options": "Customer",
+            "label": __("Customer")
+        },
+        {
+            "fieldtype": "Link",
+            "fieldname": "responsible",
+            "options": "Responsible",
+            "label": __("Responsible")
+        }
+    ],
+    get_events_method: "psag.psag.doctype.service_event.service_event.get_service_events"
 }
